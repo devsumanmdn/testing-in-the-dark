9c02f8e17208c5ad9d6e2ce0657babe9d4454fd0
Suman Kundu <devsumanmdn@gmail.com>
2019-10-03 09:20:41 +0530

    view booked slots

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,17 +1,58 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
+import styled from "styled-components";
+import moment from "moment-timezone";
 import { connect } from "react-redux";
 
-const index = ({ auth }) => (
+const SlotButton = styled.button`
+  background-color: #fff;
+  border: 2px solid #ddd;
+  border-radius: 3px;
+  padding: 5px;
+  margin: 10px;
+  transition-duration: 0.3s;
+  cursor: pointer;
+
+  &.selected.occupied {
+    background-color: #22f;
+    color: #fff;
+  }
+
+  &.occupied {
+    background-color: #aaa;
+    cursor: not-allowed;
+  }
+
+  :hover {
+    background-color: #22f6;
+    /* color: #fff; */
+  }
+`;
+
+const index = ({ auth, slots }) => (
   <>
     <Head>
       <script src={"/static/js/main.js"} />
     </Head>
-    <main>hi {auth.loggedIn ? auth.user.name : "Anonymous"}</main>
+    <main>
+      <p>hi {auth.loggedIn ? auth.user.name : "Anonymous"}</p>
+      {auth.loggedIn
+        ? slots.ownSlots.map(({ startTime, endTime }) => {
+            const userTimeZone =
+              auth.user && auth.user.timeZone
+                ? auth.user.timeZone
+                : "Asia/Calcutta";
+            const startDate = new Date(startTime);
+            const slotmoment = moment(startDate, userTimeZone);
+            const formatted = slotmoment.format("hh a");
+            return <SlotButton key={formatted}>{formatted}</SlotButton>;
+          })
+        : null}
+    </main>
   </>
 );
 
-const mapStateToProps = ({ auth }) => ({ auth });
+const mapStateToProps = ({ auth, slots }) => ({ auth, slots });
 
 export default connect(mapStateToProps)(index);
